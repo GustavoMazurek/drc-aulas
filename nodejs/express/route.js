46e40a6a4892c1547486e267b98ddd4b39bb6cef
Gustavo Mazurek <seunome@email.com>
2015-06-19 22:01:59 -0300

    add NodeJS Get Push Put Delet

--- nodejs/express/route.js
+++ nodejs/express/route.js
@@ -1,11 +1,20 @@
 var express = require('express'),
-        app = express();
+    app = express();
 
-app.get('/', function (req, res){
+/* app.get('/', function (req, res){
     res.send('<h1>Hello From NodeJS</h1>');
+}); */
+
+app.use(express.static('public'));
+
+app.get('/service', function(req, res){
+    res.json({data: 'Some data'});
+});
+
+app.get('/service/:name/:last_name', function(req, res){
+    res.send('<h1>Hello ' + req.params.name + ' ' + req.params.last_name + '</h1>');
 });
 
 app.listen(8080);
 
 console.log('Server running on localhost:8080');
-
